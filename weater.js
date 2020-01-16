80877ffa791b4b163752a456eec7c4408869c044
seungEH <48157056+seungEH@users.noreply.github.com>
2020-01-16 20:01:00 +0900

    Getting the Weather part One(Geolocation)

---
+++ weater.js
@@ -0,0 +1,40 @@
+//api key
+const COORDS = "coords";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("can not access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null){
+        console.log("111");
+        askForCoords();      
+    }else {
+        //getWeather 
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
